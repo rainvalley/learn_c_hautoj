a4ad12048b1e0f2f9b5d2dd7049335760f9c30dd
rainvalley <huyadong1234@gmail.com>
2019-10-03 16:42:25 +0800

    correct 1055.c

--- 50_99/1055.c
+++ 50_99/1055.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
-#include<math.h>
+ 
 int main()
 {
-    int n,sum,i;
-    scanf("%d",&n);
-    sum = 1;
-    for ( i = 1; i <= n; i=3*i)
+    int i, n;
+    int preNum, curNum, temp;
+ 
+    scanf("%d", &n);
+    preNum = curNum = 1;
+ 
+    for(i = 3; i <= n; i++)
     {
-        sum = sum +
+        temp = curNum;
+        curNum = curNum + preNum;
+        preNum = temp;
     }
-    
+ 
+    printf("%d\n", curNum);
+    return 0;
 }
